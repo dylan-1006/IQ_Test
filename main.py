87d52f32918dfc8a293901f8ed19feb64bbb52d9
JunKit2003 <101944073+JunKit2003@users.noreply.github.com>
2022-03-21 09:48:36 +0800

    Added test file

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-print('Welcome to this new project.')
-print('Test 1')
-print('Test2')
+import random
+import time
 
-print('Test 3')
-print('Hello world')
+
+print('----------------------------------------------------------')
+print('-                 Welcome to IQ test                     -')
+print('----------------------------------------------------------')
+time.sleep(2)
+
+score = 0
+
+''' Questions 1 '''
+def answer1():
+    global score
+    score = 0
+    ans = str(input('Please input your answer :'))
+    if ans == 'Ban':
+        score = score + 1
+    elif ans == 'Bane' or 'Bun' or 'Band' :
+        score = score + 0
+    else:
+        print('Please enter a valid answer')
+        answer1()
+    
+def question1():
+    print('')
+    print('Question 1:')
+    print('Insert the word that completes the first word and begins the second. Ur (...) AI')
+    time.sleep(4)
+    list = ['Band', 'Bun', 'Bane', 'Ban']
+    random.shuffle(list)
+    for x in range(len(list)):
+        print(list[x])
+    time.sleep(4)
+    answer1()
+ 
+question1()
+print(score)
