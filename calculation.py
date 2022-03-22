23330582d4457968c6a34096aa6d4d6d69047bd9
Dylan Eu <eu.dylan@gmail.com>
2022-03-22 19:21:09 +0800

    Added 2 functions: basic_score()   - to calculate score for when user chooses basic mode. complex_score() - to calculate score for when user chooses complex mode.
    # the code is not completed yet but the only thing left to do is to add the remaining correct answers in the CORRECT_ANS list.

--- calculation.py
+++ calculation.py
@@ -1,1 +1,29 @@
+# score calculator for basic mode
+def basic_score(user_ans):
+    CORRECT_ANS = ['B', 'D', 'B', '197', 'RUNNER', '35']
+    position = 0
+    score = 0
+    for answer in user_ans:
+        if answer == CORRECT_ANS[position]:
+            score += 1
+            print(score)
+
+        position += 1
+
+    return score
+
+
+# score calculator for complex mode
+def complex_score(user_ans):
+    CORRECT_ANS = ['B', 'D', 'B', '197', 'RUNNER', '35']
+    position = 0
+    score = 0
+    for answer in user_ans:
+        if answer == CORRECT_ANS[position]:
+            score += 1
+            print(score)
+
+        position += 1
+
+    return score
 
