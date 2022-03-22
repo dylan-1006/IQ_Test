e9be2345f2a3a1b9dcdac5a7767cf27804e64026
Dylan Eu <eu.dylan@gmail.com>
2022-03-22 17:47:29 +0800

    Added 5 functions to verify data type and print error message: slowprint() - prints words slowly abcd()      - checks if input is abcd isword()    - checks if input is alphabets only isnumber()  - checks if input is digits only

--- verify.py
+++ verify.py
@@ -1,1 +1,47 @@
+import sys
+import time
 
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1. / 10)
+
+
+def abcd(answer):
+    verify = False
+    if answer.upper() == 'A' or answer.upper() == 'B' or answer.upper() == 'C' or answer.upper() == 'D':
+        verify = True
+
+    else:
+        verify = False
+
+    return verify
+
+
+def isword(answer):
+    verify = False
+    if answer.isalpha():
+        verify = True
+
+    else:
+        verify = False
+
+    return verify
+
+
+def isnumber(answer):
+    verify = False
+    if answer.isalpha() is False:
+        verify = True
+
+    else:
+        verify = False
+
+    return verify
+
+
+def errormsg(verify, answer):
+    if verify is False and answer != '':
+        slowprint('Incorrect input type \n\n')
