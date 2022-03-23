0194a02c38aa74c26b4bbc30a9ecf9e844ea12c2
Dylan Eu <eu.dylan@gmail.com>
2022-03-23 19:07:55 +0800

    Added 2 functions: initialise() - allows the user to choose between the two modes              - displays iq and the percentile it belongs to
    retry()      - allows user to choose whether to redo the IQ Test after completing it once

--- main.py
+++ main.py
@@ -1,162 +1,67 @@
-import random
+import prompt
+import calculation
+import results
 import time
+import sys
 
 print('----------------------------------------------------------')
-print('-                 Welcome to IQ test                     -')
+print('-                 Welcome to IQ Test                     -')
 print('----------------------------------------------------------')
 time.sleep(2)
 
-score = 0
 
-'''   Questions  '''
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1. / 10)
 
 
-def question1():
-    print('')
-    print('Question 1:')
-    time.sleep(2)
-    print('473982 is to 1419')
-    time.sleep(2)
-    print('329684 is to 1418')
-    time.sleep(2)
-    print('therefore')
-    print('751694 is to (...) ?')
-    time.sleep(2)
-    global score
-    ans = str(input('Please input your answer :'))
-    if ans.isdigit():
-        ans = int(ans)
-        if ans == 1319:
-            score = score + 1
+def initialise():
+    mode_verify = False
+
+    while mode_verify is False:
+        mode = input('\nPlease select the desired mode for this IQ Test [B or C]:')
+        if mode.upper() == 'B':
+            mode_verify = True
+            user_ans = prompt.prompt_basic()
+            score = calculation.basic_score(user_ans)
+
+        elif mode.upper() == 'C':
+            mode_verify = True
+            user_ans = prompt.prompt_complex()
+            score = calculation.complex_score(user_ans)
         else:
-            score = score + 0
-    else:
-        print('Please enter a digit')
+            mode_verify = False
+            print('Please enter [B] or [C] only')
+
+    iq = results.score_to_iq(score)
+    percentile = results.iq_percentile(iq)
+    slowprint('\nCalculating your IQ......\n5% \n18% \n33% \n52% \n73%')
+    time.sleep(1.5)
+    slowprint('82%')
+    time.sleep(1.5)
+    slowprint('99% \n100% \nLoading......')
+    time.sleep(2)
+    slowprint('\nYour IQ is ' + str(iq) + ' and belongs to the ' + percentile + ' percentile. \n\n')
+    retry()
 
 
-def question2():
-    print('')
-    print('Question 2:')
-    time.sleep(2)
-    print('Which word in brackets is closest in meaning to the word in capitals?')
-    time.sleep(5)
-    print('ESPOUSAL')
-    time.sleep(2)
-    print('(reverence, adoption, outbreak, opinion, invitation)?')
-    time.sleep(2)
-    global score
-    ans = str(input('Please input your answer :'))
-    if ans.isalpha():
-        ans = ans.upper()
-        if ans == 'ADOPTION':
-            score = score + 1
+def retry():
+    retry_verify = False
+    while retry_verify is False:
+        startover = input('Do you wish to retry this IQ Test [Y or N] ? :')
+        if startover.upper() == 'Y':
+            retry_verify = True
+            initialise()
+
+        elif startover.upper() == 'N':
+            retry_verify = True
+            slowprint('Thanks for using this IQ test. Have a nice day!')
+
         else:
-            score = score + 0
-    else:
-        print('Please enter a word from the bracket.')
+            retry_verify = False
+            print('Please enter [Y] or [N] only.')
 
 
-def question3():
-    print('')
-    print('Question 3:')
-    time.sleep(2)
-    print('Solve the anagram in brackets to complete the quotation')
-    time.sleep(5)
-    print('Writing about music is like dancing about')
-    print('(ERECT HAIRCUT)')
-    time.sleep(2)
-    global score
-    ans = str(input('Please input your answer :'))
-    if ans.isalpha():
-        ans = ans.upper()
-        if ans == 'ARCHITECTURE':
-            score = score + 1
-        else:
-            score = score + 0
-    else:
-        print('Please enter a word.')
-
-
-def question4():
-    print('')
-    print('Question 4:')
-    time.sleep(2)
-    print('What is the meaning of sedition?')
-    time.sleep(5)
-    print('A : responsive to stimuli')
-    time.sleep(2)
-    print('B : inducing calmness')
-    time.sleep(2)
-    print('C : relating to drinks')
-    time.sleep(2)
-    print('D : rebellious speech or action')
-    time.sleep(2)
-    print('E : deposit of rock fragments')
-    global score
-    ans = str(input('Please input your answer [A,B,C,D]:'))
-    if ans.isalpha():
-        ans = ans.upper()
-        if ans == 'D':
-            score = score + 1
-        else:
-            score = score + 0
-    else:
-        print('Please enter a word from the bracket.')
-
-
-def question5():
-    print('')
-    print('Question 5:')
-    time.sleep(2)
-    print('Which is the odd one out ?')
-    time.sleep(3)
-    print('bow, portal, rose, bay, lancet')
-    time.sleep(2)
-    global score
-    ans = str(input('Please input your answer :'))
-    if ans.isalpha():
-        ans = ans.upper()
-        if ans == 'PORTAL':
-            score = score + 1
-        else:
-            score = score + 0
-    else:
-        print('Please enter a word.')
-
-
-def question6():
-    print('')
-    print('Question 6:')
-    time.sleep(2)
-    print('975, 319, 753')
-    time.sleep(3)
-    print('What continues the above sequence ?')
-    time.sleep(2)
-    global score
-    ans = str(input('Please input your answer :'))
-    if ans.isdigit():
-        ans = int(ans)
-        if ans == '197':
-            score = score + 1
-        else:
-            score = score + 0
-    else:
-        print('Please enter a word.')
-
-
-''' retry function '''
-
-
-def retry(a1, b1):
-    startover = input('Do you want to retry this question [Y or N] ?')
-    startover = startover.upper()
-    if startover == 'Y':
-        a1
-    else:
-        conti = input('Do you wish to continue to the next question or end the test? [next / end] :')
-        conti = conti.upper()
-        if conti == 'NEXT':
-            b1
-        else:
-            print('Thanks for trying out IQ test')
-
+initialise()
