ca5d6f7ff0b2c63d4955d3987cbe7e0cc92e6387
Dylan Eu <eu.dylan@gmail.com>
2022-03-29 15:09:57 +0800

    Final version 1.0
    # Will not edit again unless bug found

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def initialise():
     mode_verify = False
 
     while mode_verify is False:
-        mode = input('\nPlease select the desired mode for this IQ Test [B or C]:')
+        mode = input('\nPlease select the desired mode for this IQ Test [B (Basic) or C (Complex)]:')
         if mode.upper() == 'B':
             mode_verify = True
             user_ans = prompt.prompt_basic()
